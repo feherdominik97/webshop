fdc572019ef7560fdc4efd04c04bf84cc37c7c8b
Dominik Fehér <feherdominik97@gmail.com>
2024-06-24 13:20:39 +0200

    sum calculation test

--- tests/Unit/CartTest.php
+++ tests/Unit/CartTest.php
@@ -11,49 +11,47 @@ use PHPUnit\Framework\TestCase;
  */
 class CartTest extends TestCase
 {
+    private $products = [
+        [
+            'id' => 3002,
+            'name' => 'a',
+            'price' => 1000.0,
+            'newPrice' => 1000.0,
+            'manufacturer' => 'ASUS'
+        ],
+        [
+            'id' => 3003,
+            'name' => 'a',
+            'price' => 1000.0,
+            'newPrice' => 1000.0,
+            'manufacturer' => 'ASUS'
+        ],
+        [
+            'id' => 3004,
+            'name' => 'a',
+            'price' => 1000.0,
+            'newPrice' => 500.0,
+            'manufacturer' => 'ASUS'
+        ]
+    ];
+
     /**
      * @return void
      */
     public function testClassConstructor()
     {
-        $products = [
-            [
-                'id' => 3002,
-                'name' => 'a',
-                'price' => 1000.0,
-                'newPrice' => 1000.0,
-                'manufacturer' => 'ASUS'
-            ],
-            [
-                'id' => 3003,
-                'name' => 'a',
-                'price' => 1000.0,
-                'newPrice' => 1000.0,
-                'manufacturer' => 'ASUS'
-            ],
-            [
-                'id' => 3004,
-                'name' => 'a',
-                'price' => 1000.0,
-                'newPrice' => 500.0,
-                'manufacturer' => 'ASUS'
-            ]
-        ];
-
-        $cart = new Cart($products);
-
+        $cart = new Cart($this->products);
+        
         $sum = 0;
-        foreach ($products as $product) {
+        foreach ($this->products as $product) {
             $sum += $product['newPrice'];
         }
 
-        foreach ($products as $array_key => $product_array) {
+        foreach ($this->products as $array_key => $product_array) {
             foreach ($product_array as $property => $value) {
                 $this->assertSame($value, $cart->getProducts()[$array_key]->{'get' . ucfirst($property)}());
             }
         }
-
-        $sum = round($sum);
 
         $this->assertSame($sum, $cart->getSum());
     }
@@ -63,31 +61,7 @@ class CartTest extends TestCase
      */
     public function testAddProduct()
     {
-        $products = [
-            [
-                'id' => 3002,
-                'name' => 'a',
-                'price' => 1000.0,
-                'newPrice' => 1000.0,
-                'manufacturer' => 'ASUS'
-            ],
-            [
-                'id' => 3003,
-                'name' => 'a',
-                'price' => 1000.0,
-                'newPrice' => 1000.0,
-                'manufacturer' => 'ASUS'
-            ],
-            [
-                'id' => 3004,
-                'name' => 'a',
-                'price' => 1000.0,
-                'newPrice' => 500.0,
-                'manufacturer' => 'ASUS'
-            ]
-        ];
-
-        $cart = new Cart($products);
+        $cart = new Cart($this->products);
 
         $product_to_add = [
             'id' => 3005,
@@ -100,12 +74,10 @@ class CartTest extends TestCase
         $cart->addProduct(new Product($product_to_add));
 
         $sum = 0;
-        foreach ($products as $product) {
+        foreach ($this->products as $product) {
             $sum += $product['newPrice'];
         }
-        $sum = round($sum);
         $sum += $product_to_add['newPrice'];
-        $sum = round($sum);
 
         $this->assertSame($cart->getSum(), $sum);
     }
@@ -115,43 +87,31 @@ class CartTest extends TestCase
      */
     public function testRemoveProduct()
     {
-        $products = [
-            [
-                'id' => 3002,
-                'name' => 'a',
-                'price' => 1000.0,
-                'newPrice' => 1000.0,
-                'manufacturer' => 'ASUS'
-            ],
-            [
-                'id' => 3003,
-                'name' => 'a',
-                'price' => 1000.0,
-                'newPrice' => 1000.0,
-                'manufacturer' => 'ASUS'
-            ],
-            [
-                'id' => 3004,
-                'name' => 'a',
-                'price' => 1000.0,
-                'newPrice' => 500.0,
-                'manufacturer' => 'ASUS'
-            ]
-        ];
-
-        $cart = new Cart($products);
+        $cart = new Cart($this->products);
 
         $key_to_remove = 1;
 
         $cart->removeProduct($key_to_remove);
 
         $sum = 0;
-        foreach ($products as $product) {
+        foreach ($this->products as $product) {
             $sum += $product['newPrice'];
         }
-        $sum = round($sum);
-        $sum -= $products[$key_to_remove]['newPrice'];
-        $sum = round($sum);
+        $sum -= $this->products[$key_to_remove]['newPrice'];
+
+        $this->assertSame($cart->getSum(), $sum);
+    }
+
+    public function testCalculateSum()
+    {
+        $cart = new Cart($this->products);
+
+        $cart->calculateSum();
+
+        $sum = 0;
+        foreach ($this->products as $product) {
+            $sum += $product['newPrice'];
+        }
 
         $this->assertSame($cart->getSum(), $sum);
     }
